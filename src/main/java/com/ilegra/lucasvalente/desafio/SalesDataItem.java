68d7af35c18bb1e9ecc9552e3614b9d2d929c729
Lucas <lucasvalenteds@gmail.com>
2018-03-12 19:19:00 -0300

    Implements boilerplate methods in SalesDataItem
    toString, equals and hashcode.

--- src/main/java/com/ilegra/lucasvalente/desafio/SalesDataItem.java
+++ src/main/java/com/ilegra/lucasvalente/desafio/SalesDataItem.java
@@ -1,5 +1,7 @@
 package com.ilegra.lucasvalente.desafio;
 
+import java.util.Objects;
+
 class SalesDataItem {
 
     private final String id;
@@ -22,5 +24,21 @@ class SalesDataItem {
 
     public double getPrice() {
         return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SalesDataItem that = (SalesDataItem) o;
+        return quantity == that.quantity &&
+                Double.compare(that.price, price) == 0 &&
+                Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, quantity, price);
     }
 }
