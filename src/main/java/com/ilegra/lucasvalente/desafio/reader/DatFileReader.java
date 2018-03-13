a080048fadf968c7d1bcefcc1738973ee6afbfd9
Lucas <lucasvalenteds@gmail.com>
2018-03-12 21:15:21 -0300

    Remove unused imports from class DatFileReader

--- src/main/java/com/ilegra/lucasvalente/desafio/reader/DatFileReader.java
+++ src/main/java/com/ilegra/lucasvalente/desafio/reader/DatFileReader.java
@@ -2,11 +2,8 @@ package com.ilegra.lucasvalente.desafio.reader;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardWatchEventKinds;
