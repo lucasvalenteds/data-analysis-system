37986577bf69610fc430b6fe246e309dc526a6bc
Lucas <lucasvalenteds@gmail.com>
2018-09-17 11:27:19 -0300

    chore: Update project files
    chore: Improve Docker usage and versioning
    
    * Rewrite Dockerfile to run on JDK 11
    * Update GitLab CI scripts to run on JDK 11
    * Add `.bmp.yml` file to make versioning easily
    
    chore: Refactoring build scripts
    
    * Upgrade from JDK 9 to JDK 11
    * Upgrade from Gradle 4.6 to 4.10.1
    * Migrate Gradle scripts to Kotlin DSL
    * Upgrade JaCoCo to SNAPSHOT to support JDK 11
    * Add task to generate Gradle Wrapper
    * Add plugin to manage dependencies version
    
    Fixes #1

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM openjdk:9-slim
+FROM gradle:4.10.1-jdk11-slim as builder
+COPY --chown=gradle:gradle . /home/gradle/src
+WORKDIR /home/gradle/src
+RUN gradle --no-daemon shadowJar
 
+FROM openjdk:11-jre-slim
+COPY --from=builder /home/gradle/src/build/libs/lucasvalente-0.1.0.jar /app/app.jar
 WORKDIR /app
-
-COPY ./ ./
-
-RUN ./gradlew clean jar
-
-CMD java -jar build/libs/lucasvalente-0.1.0.jar
-
+VOLUME ["/tmp"]
+CMD ["java", "-Djava.security.egd=file:/dev/./urandom", "-jar", "app.jar"]
---
+++ build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    java
+    application
+    jacoco
+    id("com.adarshr.test-logger") version "1.5.0"
+    id("com.github.ben-manes.versions") version "0.20.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.3"
+}
+
+group = "com.ilegra.lucasvalente"
+version = "0.1.0"
+
+repositories {
+    jcenter()
+    maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+}
+
+dependencies {
+    implementation("org.javatuples:javatuples:1.2")
+    testImplementation("org.mockito:mockito-core:2.22.0")
+    testImplementation("org.assertj:assertj-core:3.11.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.3.1")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+}
+
+configure<ApplicationPluginConvention> {
+    mainClassName = "com.ilegra.lucasvalente.Main"
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+configure<JacocoPluginExtension> {
+    toolVersion = "0.8.3-SNAPSHOT"
+}
+
+tasks.withType<JavaCompile> {
+    options.compilerArgs = listOf("-proc:none")
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes(
+            "Class-Path" to configurations.compile.all.map(Configuration::getName).joinToString(" "),
+            "Main-Class" to "$group.Main"
+        )
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    failFast = true
+    finalizedBy(tasks.withType<JacocoReport>())
+}
+
+tasks.withType<JacocoReport> {
+    reports {
+        csv.isEnabled = false
+        xml.isEnabled = true
+        html.isEnabled = true
+    }
+}
+
+tasks.withType<Wrapper> {
+    distributionType = Wrapper.DistributionType.ALL
+}
