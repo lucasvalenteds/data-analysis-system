00920190b3185ca95703be60ce0de3bf5497a932
Lucas <lucasvalenteds@gmail.com>
2019-09-21 18:08:21 -0300

    Rewrite Gradle build script
    * Remove usage of `buildSrc` mechanism
    * Remove code quality plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,13 @@
-import com.adarshr.gradle.testlogger.TestLoggerExtension
-import io.lucasvalenteds.plugin.setupApplication
-import io.lucasvalenteds.plugin.setupGradleWrapper
-import io.lucasvalenteds.plugin.setupJacoco
-import io.lucasvalenteds.plugin.setupJar
-import io.lucasvalenteds.plugin.setupJava
-import io.lucasvalenteds.plugin.setupJunit
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 group = "io.lucasvalenteds"
 version = "0.2.1"
 
 plugins {
     java
+    application
+    id("com.github.johnrengelman.shadow") version "5.1.0"
 }
-
-setupApplication()
-setupJava()
-setupJacoco()
-setupJunit()
-setupJar()
-setupGradleWrapper()
 
 repositories {
     jcenter()
@@ -36,3 +25,20 @@ dependencies {
     testImplementation("org.junit.jupiter", "junit-jupiter-params", extra["version.junit"].toString())
     testRuntime("org.junit.jupiter", "junit-jupiter-engine", extra["version.junit"].toString())
 }
+
+configure<ApplicationPluginConvention> {
+    mainClassName = "io.lucasvalenteds.batch.Main"
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events = setOf(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
+    }
+}
+
