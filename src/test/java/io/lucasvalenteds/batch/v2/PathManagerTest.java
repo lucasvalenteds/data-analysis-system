9bccf6370a8c53c24f6b41b7c185ba24c2a75178
Lucas <lucasvalenteds@gmail.com>
2019-09-21 18:22:13 -0300

    Add test suite to PathManager

---
+++ src/test/java/io/lucasvalenteds/batch/v2/PathManagerTest.java
@@ -0,0 +1,28 @@
+package io.lucasvalenteds.batch.v2;
+
+import java.nio.file.Path;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class PathManagerTest {
+
+    private final PathManager manager = new PathManager();
+
+    @Test
+    void testResolveReportName() {
+        var input = Path.of("foo.dat");
+
+        var output = manager.resolveReportFilename(input.getFileName());
+
+        assertTrue(output.toString().endsWith(".done.dat"));
+    }
+
+    @Test
+    void testHasDatExtension() {
+        assertTrue(manager.hasDatExtension(Path.of("bar.dat")));
+        assertFalse(manager.hasDatExtension(Path.of("bar.txt")));
+        assertFalse(manager.hasDatExtension(Path.of("bar.dAt")));
+        assertFalse(manager.hasDatExtension(Path.of("bar.dat.not")));
+    }
+}
